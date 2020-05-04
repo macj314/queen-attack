1fba64dc9cae3207881af1bf37651aace5414bce
unknown <macjay314@gmail.com>
2020-05-04 16:13:45 -0700

    Create skeleton of Queen move function and x,y coordinate collection from the user

--- QueenAttack.cs
+++ QueenAttack.cs
@@ -2,9 +2,15 @@ using System;
 
 class QueenAttack
 {
-
+  /* Create a Queen class to handle x, and y coordinate inputs. This should return a boolean
+  Create an 8x8 board using nested arrays.
+  */
   static void Main()
   {
-
+    Console.Write("Your chess game is in session. Your queen is in the front lines of your side: x:4, y7. Please input a pair of x, y coordinates to see if you can move there with your queen.");
+    Console.Write("x");
+    int x = Console.Read();
+    Console.Write("y");
+    int y = Console.Read();
   }
 }
